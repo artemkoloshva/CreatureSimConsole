c79849445860498b1ec252758d17ce2e1421dbb8
artemkoloshva <artemkoloshva11yt@gmail.com>
2026-08-20 19:33:56 +0400

    feat: add Position record

---
+++ src/com/github/artemkoloshva/model/Position.java
@@ -0,0 +1,21 @@
+package com.github.artemkoloshva.model;
+
+import java.util.Objects;
+
+public record Position(int x, int y) {
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Position position = (Position) o;
+
+        return x == position.x && y == position.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+}
